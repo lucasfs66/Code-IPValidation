9975c658057d077ec8865e08b5a2b5eab1f5f1da
Lucas <lucasfreiresabino@gmail.com>
2023-01-25 13:23:30 -0700

    in progress

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+Write an algorithm that will identify valid IPv4 addresses in 
+dot-decimal format. IPs should be considered valid if they 
+consist of four octets, with values between 0 and 255, inclusive.
+
+*/
+
+
+function isValidIP(str) {
+    
+    return false;
+  }
+
+  isValidIP("0.0.0.0")
+  isValidIP("137.255.156.100")
